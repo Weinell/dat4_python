9417ea424dd9fb7bb2f739c2d2a8afe7132335ba
Weinell Mac <nikolajweinell@hotmail.com>
2022-09-05 13:34:49 +0200

    Restructure folders

---
+++ notebooks/_my_modules/utils.py
@@ -0,0 +1,31 @@
+
+import argparse
+
+
+def get_file_names():#, out='output.txt'):
+    folderpath = '/Users/weinell/Documents/Dev/CPH Business/4. Semester/Python/docker_notebooks/notebooks/my_notebooks/Week 2'
+    print(folderpath)
+    return
+
+def get_all_file_names(folderpath, out='output.txt'):
+    return
+
+def print_line_one(file_names):
+    return
+
+def print_emails(file_names):
+    return
+
+def write_headlines(md_files, out='output.txt'):
+    return
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    get_file_names()
